c0b051150f87144135de2ae65b4bff02bcc26d99
srive <srive@DESKTOP-O3BL95U>
2020-11-21 12:09:45 +0530

    Initial Project setup

---
+++ src/main/java/me/learning/mybeerclient/MybeerclientApplication.java
@@ -0,0 +1,13 @@
+package me.learning.mybeerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybeerclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybeerclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/learning/mybeerclient/MybeerclientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.learning.mybeerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybeerclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
